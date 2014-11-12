bb7ec41e4de3fa6110d131f59ad45270816491b1
klob <185902156@qq.com>
2014-11-12 21:35:27 +0800

    liuping

--- src/network/user/Login.java
+++ src/network/user/Login.java
@@ -4,7 +4,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * Created by Administrator on 2014/11/6 0006.
@@ -15,14 +14,15 @@ public class Login extends UserService {
         req.setCharacterEncoding("UTF-8");
         mUser = mBussiness.findByName(mUserName);
         if (mUser == null) {
+            mAddress = "404.jsp";
             System.out.println("用户不存在");
         } else if (!mUser.getUserPsd().equals(mUserPsd)) {
             System.out.println("密码错误");
         } else {
             System.out.println("登录成功 User : " + mUser);
             mAddress = "find.jsp";
-            req.getRequestDispatcher(mAddress).forward(req, resp);
         }
+        req.getRequestDispatcher(mAddress).forward(req, resp);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
